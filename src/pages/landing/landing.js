e59382320abe727416e65cca9548fd453d3c160e
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-09 23:19:08 +0000

    Strip out the default

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
-import './stylesheets/landing'
-import Portfolio from '../../components/portfolio/portfolio'
-import { StickyContainer } from 'react-sticky';
+import './landing.scss'
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        <StickyContainer>
-          <Portfolio />
-        </StickyContainer>
+        Hello
       </div>
     )
   }
