b726fa34a9b0fc779e8fa5b8fc95c76f5055b66d
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-10 04:33:02 +0000

    Makes the drop keyboard controlled

--- src/components/drone/drone.js
+++ src/components/drone/drone.js
@@ -1,10 +1,60 @@
-import React from 'react'
-import './drone.scss'
+import React from "react"
+import "./drone.scss"
 
 export default class Drone extends React.Component {
+
+  state = {
+    left: 50,
+    top: 50,
+  }
+
+  componentDidMount() {
+    document.onkeydown = (e) => {
+      switch (e.keyCode) {
+        case 37: // left
+          this.move("left", -3)
+          break
+        case 39: // right
+          this.move("left", 3)
+          break
+        case 38: // top
+          this.move("top", -3)
+          break
+        case 40: // top
+          this.move("top", 3)
+          break
+      }
+    }
+  }
+
+  move = (direction, amount) => {
+    let
+      left = this.state.left,
+      top = this.state.top
+
+    if (direction === "left" || direction === "right") {
+      left = this.state.left + amount
+    } else {
+      top = this.state.top + amount
+    }
+
+    this.setState({
+      left,
+      top,
+    })
+  }
+
   render() {
+    let style = {
+      left: this.state.left + "%",
+      top: this.state.top + "%",
+    }
     return (
-      <div className="drone"></div>
+      <div
+        ref="drone"
+        style={style}
+        onKeyPress={this.onKey}
+        className="drone" />
     )
   }
 }
