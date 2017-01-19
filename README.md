d8409ac241c870bfffd22a7d6e48e03e74fe46fa
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-19 19:08:12 +0000

    Reading

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-* Build once:
-    * `npm run build`
-    * Open `build/index.html`
-* Watch files continuously, rebuild incrementally, whenever one of them changes:
-    * `npm run watch`
-    * Open `build/index.html`, manually reload page in browser whenever there was a change
-* Hot reloading via webpack dev server:
-    * `npm start`
-    * Go to `http://localhost:8080/`, page reloads automatically when there are changes
+* Build: `npm run build`
+* Watch files continuously, rebuild whenever one of them changes: `npm run watch`
+* Hot reloading via webpack dev server: `npm start`
+
+http://localhost:8080/
+
+es6 js
+babel, node
+tests with karma mocha chai enzyme
+spa: 5 day weather forecast for a location
+
+OpenWeatherMap 5 day weather forecast API: http://openweathermap.org/forecast5
+Readable but with no need to go all out on sleek and flashy UI elements.
--- src/main.js
+++ src/main.js
@@ -1,14 +1,56 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import "whatwg-fetch"
+const settings = {
+  appId: "c7074b48826a3e1f5def36267b52c7bb",
+  apiUrl: "http://api.openweathermap.org/data/2.5/forecast"
+}
 
 class App extends React.Component {
+
+  state = {
+    q: "London, UK",
+    mode: "json",
+    results: [],
+  }
+
+  componentDidMount() {
+    const url = `${settings.apiUrl}?appId=${settings.appId}&q=${this.state.q}&mode=${this.state.mode}`
+    fetch(url, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then((results) => {
+      this.setState({
+        results,
+      })
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   render() {
+    console.log("state", (this.state.results))
     return (
       <div>
-        Our new React App
+        {this.state.results.map((result) => {
+          return (
+            <div style={{backgroundColor: "red"}}>
+              {result.weather}
+            </div>
+          )
+        })}
       </div>
     )
   }
 }
 
 ReactDOM.render(<App />, document.getElementById("container"))
+
+/*
+{this.state.results.map((result, key) => {
+  return (
+    <div key={key}>{key}</div>
+  )
+})}
+*/
