36f2425828355995b7d62925e973a85c2cea14ec
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2017-01-20 16:37:49 +0000

    Times component

--- src/components/day/day.js
+++ src/components/day/day.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { Times } from "../times/times"
+
 const defaultTimeSettings = {
   hour: "2-digit",
   minute: "2-digit",
@@ -22,37 +24,9 @@ export const Day = ({
       <h3 className="day__title">
         {date.toLocaleDateString([], dateSettings)}
       </h3>
-      <section className="times">
-        {times.map((time, key) =>
-          <div className="time" key={key}>
-            <span className="time__time">
-              {time.dateTime.toLocaleTimeString([], timeSettings)}
-            </span>
-            <span className="time__main">
-              {time.main}&nbsp;
-              <Choose>
-                <When condition={time.main === "Clouds"}>
-                  <span className="icon fa fa-cloud"></span>
-                </When>
-                <When condition={time.main === "Clear"}>
-                  <span className="icon clear fa fa-cloud"></span>
-                </When>
-                <When condition={time.main === "Rain"}>
-                  <span className="icon clear fa fa-umbrella"></span>
-                </When>
-                <Otherwise>
-                </Otherwise>
-              </Choose>
-            </span>
-            <span className="time__description">
-              {time.description}
-            </span>
-            <div>
-              <hr className="separator" />
-            </div>
-          </div>
-        )}
-      </section>
+      <Times times={times}
+        timeSettings={timeSettings}
+      />
     </div>
   )
 }
---
+++ src/components/times/times.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+export const Times = ({
+  times = [],
+  timeSettings = {},
+}) => {
+  return (
+    <section className="times">
+      {times.map((time, key) =>
+        <div key={key}
+          className="time">
+          <span className="time__time">
+            {time.dateTime.toLocaleTimeString([], timeSettings)}
+          </span>
+          <span className="time__main">
+            {time.main}&nbsp;
+            <Choose>
+              <When condition={time.main === "Clouds"}>
+                <span className="icon fa fa-cloud"></span>
+              </When>
+              <When condition={time.main === "Clear"}>
+                <span className="icon clear fa fa-cloud"></span>
+              </When>
+              <When condition={time.main === "Rain"}>
+                <span className="icon clear fa fa-umbrella"></span>
+              </When>
+              <Otherwise>
+              </Otherwise>
+            </Choose>
+          </span>
+          <span className="time__description">
+            {time.description}
+          </span>
+          <div>
+            <hr className="separator" />
+          </div>
+        </div>
+      )}
+    </section>
+  )
+}
